2c3ed3203af2c92f86ec8afdc761e865e4c910e4
denandserg <denandserg@gmail.com>
2018-11-16 21:45:05 +0200

    AutoShop Signed-off-by: denandserg <denandserg@gmail.com>

---
+++ AutoShop/Cars.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoShop
+{
+    class AutoShop : IComparable<AutoShop>
+    {
+
+        public string CarName; 
+        public int MaxSpeed { get; set; }
+        public double Cost { get; set; }
+        public byte Discount { get; set; }
+        public int ID { get; set; }
+
+        public AutoShop()
+        {
+            Console.WriteLine("Введите идентификатор машины => ");
+            this.ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите марку машины => ");
+            this.CarName = Console.ReadLine();
+            Console.WriteLine("Введите максимальную скорость машины => ");
+            this.MaxSpeed = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стоимость машины => ");
+            this.Cost = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите максимальную скидку для машины => ");
+            this.Discount = Convert.ToByte(Console.ReadLine());
+
+        }
+
+        // Реализуем интерфейс IComparable<T>
+        public int CompareTo(AutoShop obj)
+        {
+            if (this.Cost > obj.Cost)
+                return 1;
+            if (this.Cost < obj.Cost)
+                return -1;
+            else
+                return 0;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{4}\tМарка: {0}\t\tМакс. скорость км.час: {1}\t\tЦена: {2:C}\t\tСкидка: {3}%",
+                this.CarName, this.MaxSpeed, this.Cost, this.Discount, this.ID);
+        }
+    }
+}
---
+++ AutoShop/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AutoShop
+{
+    
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(new string('$', 60));
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var culture = new System.Globalization.CultureInfo("us-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            List<AutoShop> dic = new List<AutoShop>();
+
+            // Создадим множество автомобилей
+
+            Console.WriteLine("Просмотреть склад автомобилей - нажмите 1\nДобавить новый автомобиль - нажмите 2");
+            int q = Convert.ToInt32(Console.ReadLine());
+            if (q == 2)
+            {
+                dic.Add(new AutoShop());
+                goto label;
+            }
+
+            else goto label;
+            label:
+            Console.WriteLine("Исходный каталог автомобилей: \n");
+            foreach (AutoShop a in dic)
+                Console.WriteLine(a);
+
+            Console.WriteLine("\nТеперь автомобили отсортированны по стоимости: \n");
+            dic.Sort();
+            foreach (AutoShop a in dic)
+                Console.WriteLine(a);
+
+            Console.WriteLine();
+            Console.WriteLine("Добавить еще одну машину - нажмите 1");
+            int w = Convert.ToInt32(Console.ReadLine());
+            if (w == 1)
+            {
+                dic.Add(new AutoShop());
+                goto label;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
